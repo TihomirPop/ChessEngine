18c3039be182f535dbe49217d93389186f34330f
TihomirPop <95313780+TihomirPop@users.noreply.github.com>
2024-07-28 21:28:38 +0200

    Add pieces

---
+++ src/main/java/hr/tvz/popovic/chessengine/model/Pieces.java
@@ -0,0 +1,17 @@
+package hr.tvz.popovic.chessengine.model;
+
+public enum Pieces {
+    EMPTY,
+    WHITE_KING,
+    WHITE_ROOK,
+    WHITE_BISHOP,
+    WHITE_QUEEN,
+    WHITE_KNIGHT,
+    WHITE_PAWN,
+    BLACK_KING,
+    BLACK_ROOK,
+    BLACK_BISHOP,
+    BLACK_QUEEN,
+    BLACK_KNIGHT,
+    BLACK_PAWN
+}
