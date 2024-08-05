6c3ceefbf4a491a49166274cbb3c69c07c8be61d
TihomirPop <95313780+TihomirPop@users.noreply.github.com>
2024-08-05 23:37:56 +0200

    implement bishop generator

--- src/main/java/hr/tvz/popovic/chessengine/generator/BishopGenerator.java
+++ src/main/java/hr/tvz/popovic/chessengine/generator/BishopGenerator.java
@@ -1,17 +1,22 @@
 package hr.tvz.popovic.chessengine.generator;
 
 import hr.tvz.popovic.chessengine.model.Board;
+import hr.tvz.popovic.chessengine.model.Direction;
 import hr.tvz.popovic.chessengine.model.Move;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @Component
 class BishopGenerator extends SlidingGenerator {
 
     @Override
     public List<Move> from(Board board, int from) {
-        return List.of();
+        return Stream.of(Direction.UP_LEFT, Direction.UP_RIGHT, Direction.DOWN_LEFT, Direction.DOWN_RIGHT)
+                .map(direction -> generateSlidingMoves(board, from, direction))
+                .flatMap(List::stream)
+                .toList();
     }
 
 }
