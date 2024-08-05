651087f1a7c6015e623f1c736a456d740b1a1ae6
TihomirPop <95313780+TihomirPop@users.noreply.github.com>
2024-08-05 23:37:08 +0200

    implement rook generator

--- src/main/java/hr/tvz/popovic/chessengine/generator/RookGenerator.java
+++ src/main/java/hr/tvz/popovic/chessengine/generator/RookGenerator.java
@@ -1,17 +1,22 @@
 package hr.tvz.popovic.chessengine.generator;
 
 import hr.tvz.popovic.chessengine.model.Board;
+import hr.tvz.popovic.chessengine.model.Direction;
 import hr.tvz.popovic.chessengine.model.Move;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @Component
 class RookGenerator extends SlidingGenerator {
 
     @Override
     public List<Move> from(Board board, int from) {
-        return List.of();
+        return Stream.of(Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT)
+                .map(direction -> generateSlidingMoves(board, from, direction))
+                .flatMap(List::stream)
+                .toList();
     }
 
 }
